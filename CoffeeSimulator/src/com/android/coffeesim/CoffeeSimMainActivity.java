a6b00745269163c32a9a8b6c66f6cac135f4ac68
Tyler Leonhardt <tleonhardt@Tylers-MacBook-Pro.local>
2014-05-24 12:58:48 -0400

    Initial Commit for real this time

---
+++ CoffeeSimulator/src/com/android/coffeesim/CoffeeSimMainActivity.java
@@ -0,0 +1,43 @@
+package com.android.coffeesim;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.IGameInterface.OnCreateResourcesCallback;
+import org.andengine.ui.IGameInterface.OnCreateSceneCallback;
+import org.andengine.ui.IGameInterface.OnPopulateSceneCallback;
+import org.andengine.ui.activity.BaseGameActivity;
+
+
+
+public class CoffeeSimMainActivity extends BaseGameActivity {
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreateResources(
+			OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
