d05b4200657d17c58596a1a3c505f009c2dd8c04
Tyler Leonhardt <tleonhardt@Tylers-MacBook-Pro.local>
2014-05-24 21:19:09 -0400

    Game Manager

--- CoffeeSimulator/src/com/android/coffeesim/game/GameManager.java
+++ CoffeeSimulator/src/com/android/coffeesim/game/GameManager.java
@@ -1,8 +1,10 @@
 package com.android.coffeesim.game;
 
+import com.android.coffeesim.CoffeeSimMainActivity;
+
 public class GameManager {
 
-	public GameManager() {
+	public GameManager(CoffeeSimMainActivity activity, Engine engine, PhysicsManager physicsManager) {
 		// TODO Auto-generated constructor stub
 		
 		
